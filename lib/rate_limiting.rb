c9cbca80b54786321cd234079177e5bdf3fbed05
Emilia <e.andrzejewska@pilot.co>
2014-10-11 17:38:13 +0200

    #2 Adding new header to each response

--- lib/rate_limiting.rb
+++ lib/rate_limiting.rb
@@ -1,5 +1,15 @@
 require "rate_limiting/version"
 
 module RateLimiting
-  # Your code goes here...
+  class LimitsManager
+    def initialize(app)
+      @app = app
+    end
+    
+    def call(env)
+      status, headers, response = @app.call(env)
+      headers.merge!('X-RateLimit-Limit' => '')
+      [status, headers, response]
+    end
+  end
 end
--- spec/rate_limiting_spec.rb
+++ spec/rate_limiting_spec.rb
@@ -5,11 +5,16 @@ include Rack::Test::Methods
 describe RateLimiting do
 
   def app
-    lambda { |env| [200, {'Content-Type' => 'text/plain'}, ["Hello world!"]] }
+    RateLimiting::LimitsManager.new(lambda { |env| [200, {'Content-Type' => 'text/plain'}, ["Hello world!"]] })
   end
 
   it 'response with success' do
     get '/'
     expect last_response.ok?
+  end
+
+  it 'adds `X-RateLimit-Limit` header to a response' do
+    get '/'
+    expect(last_response.headers.keys).to include('X-RateLimit-Limit')
   end
 end
