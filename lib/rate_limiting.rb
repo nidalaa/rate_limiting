401890b24ec6d8db2cc5090c10297e4bd0e66cee
Emilia <e.andrzejewska@pilot.co>
2014-10-11 17:55:49 +0200

    #2 adding limit value in middelware constructor

--- lib/rate_limiting.rb
+++ lib/rate_limiting.rb
@@ -2,13 +2,16 @@ require "rate_limiting/version"
 
 module RateLimiting
   class LimitsManager
-    def initialize(app)
+    def initialize(app, options={})
       @app = app
+      @options = options
     end
     
     def call(env)
       status, headers, response = @app.call(env)
-      headers.merge!('X-RateLimit-Limit' => '')
+
+      limit = @options[:limit] || 60
+      headers.merge!('X-RateLimit-Limit' => limit)
       [status, headers, response]
     end
   end
--- spec/rate_limiting_spec.rb
+++ spec/rate_limiting_spec.rb
@@ -5,7 +5,7 @@ include Rack::Test::Methods
 describe RateLimiting do
 
   def app
-    RateLimiting::LimitsManager.new(lambda { |env| [200, {'Content-Type' => 'text/plain'}, ["Hello world!"]] })
+    RateLimiting::LimitsManager.new(lambda { |env| [200, {'Content-Type' => 'text/plain'}, ["Hello world!"]] }, {:limit => 247})
   end
 
   it 'response with success' do
@@ -13,8 +13,18 @@ describe RateLimiting do
     expect last_response.ok?
   end
 
-  it 'adds `X-RateLimit-Limit` header to a response' do
-    get '/'
-    expect(last_response.headers.keys).to include('X-RateLimit-Limit')
+  describe '`X-RateLimit-Limit` header' do
+    it 'is added' do
+      get '/'
+      expect(last_response.headers.keys).to include('X-RateLimit-Limit')
+    end
+
+    it 'has value from :limit in middelware constructor' do
+      app = RateLimiting::LimitsManager.new(lambda { |env| [200, {'Content-Type' => 'text/plain'}, ["Hello world!"]] }, {:limit => 247})
+      get '/'
+      expect(last_response.headers['X-RateLimit-Limit']).to eq(247)
+    end
   end
+
+  
 end
